768afb2d2dd400a6416c37278de1a23604b7a775
Dmitriy Smirnov <mygmbox26@gmail.com>
2022-06-30 06:28:42 +0500

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return parseInt(Math.abs(n)
+        .toString()
+        .split('')
+        .reverse()
+        .join(''));
 }
